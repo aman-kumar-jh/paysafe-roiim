5bdc61441598ff88d00185a7bfd067b75e4bc289
aman <15uec012@lnmiit.ac.in>
2020-07-19 16:37:50 +0530

    remove prinf statment

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
 
 	checkout = async (token, id) => {
 		//const paysafe = window.paysafe.checkout.min;
-		console.log(token);
+		//console.log(token);
 		const str =
 			"public-7751:B-qa2-0-5f031cbe-0-302d021500890ef262296563accd1cb4aab790323d2fd570d30214510bcdacdaa4f03f59477eef13f2af5ad13e3044";
 		const KEY = btoa(str);
